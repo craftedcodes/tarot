587d2b3c0afd7c044f446b8c37d5ecb932b9f712
Lea <craftedcodesberlin@gmail.com>
2023-05-02 18:16:22 +0200

    refactor to classes

--- src/main/kotlin/globalFunctions/GetIntFromString.kt
+++ src/main/kotlin/globalFunctions/GetIntFromString.kt
@@ -4,9 +4,8 @@ fun String.getInt(index : Int): Int {
 	return try {
 		this[index].digitToInt()
 	} catch (ex: Exception) {
-		println("""Deine Eingabe entspricht scheinbar nicht dem Format TT.MM.JJJJ.
-			Bitte gib nochmal dein Geburtsdatum ein.
-		""".trimIndent())
+		println("""Your entry does not appear to be in the desired format.
+			|Please enter your date again.""".trimMargin())
 		return 0
 	}
 }
