3cd29a3f8ba395548969d5e6761e2e40d14230b2
Lea <craftedcodesberlin@gmail.com>
2023-05-05 17:17:37 +0200

    hope and pray

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,53 +1,53 @@
 plugins {
-	// Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-	id("org.jetbrains.kotlin.jvm") version "1.8.10"
-	id("maven-publish")
-	
-	// Apply the java-library plugin for API and implementation separation.
-	`java-library`
+    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("maven-publish")
+    
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
 }
 
 repositories {
-	// Use Maven Central for resolving dependencies.
-	mavenCentral()
-	maven { url = uri("https://www.jitpack.io" ) }
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+    maven { url = uri("https://www.jitpack.io" ) }
 }
 
 dependencies {
-	// Use the Kotlin JUnit 5 integration.
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	
-	// Use the JUnit 5 integration.
-	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-	
-	// This dependency is exported to consumers, that is to say found on their compile classpath.
-	api("org.apache.commons:commons-math3:3.6.1")
-	
-	// This dependency is used internally, and not exposed to consumers on their own compile classpath.
-	implementation("com.google.guava:guava:31.1-jre")
-	implementation("com.github.JetBrains:gradle-intellij-plugin:1.1.2")
+    // Use the Kotlin JUnit 5 integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    
+    // Use the JUnit 5 integration.
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    api("org.apache.commons:commons-math3:3.6.1")
+    
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("com.github.JetBrains:gradle-intellij-plugin:1.1.2")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
-	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(17))
-	}
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 tasks.named<Test>("test") {
-	// Use JUnit Platform for unit tests.
-	useJUnitPlatform()
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
 }
 
 publishing {
-	publications {
-		create<MavenPublication>("maven") {
-			groupId = "com.github.craftedcodes"
-			artifactId = "tarot"
-			version = "1.0.0"
-			
-			from(components["java"])
-		}
-	}
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.craftedcodes"
+            artifactId = "tarot"
+            version = "1.0.0"
+            
+            from(components["java"])
+        }
+    }
 }
